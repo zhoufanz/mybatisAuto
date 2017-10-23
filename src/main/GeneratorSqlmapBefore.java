7b3b845e436b70c10d189a1250ce3eaf14d31c8f
周凡 <zhoufan@sunlight.bz>
2017-10-23 11:05:13 +0800

    添加 ExtJs 表单查询字段封装store查询参数工具方法

--- src/main/GeneratorSqlmapBefore.java
+++ src/main/GeneratorSqlmapBefore.java
@@ -10,10 +10,27 @@ public class GeneratorSqlmapBefore {
      */
     public static String getStringByTableList() {
         return
-                "t_base_site\nt_base_audit";
+                "T_MM_APPLICATION_DETAIL\n" +
+                        "T_MM_APPLICATION\n" +
+                        "T_MM_PURCHASE_PRICE_HISTORY\n" +
+                        "T_MM_SECTION_DETAIL\n" +
+                        "T_MM_SECTION\n" +
+                        "T_MM_PRIZE_DEPART_DETAIL\n" +
+                        "T_MM_PRIZE_DETAILE\n" +
+                        "T_MM_PRIZE\n" +
+                        "T_MM_SECTION_SL\n" +
+                        "T_MM_REPORT_FORM\n" +
+                        "T_MM_ADD_STOCK_DETAIL\n" +
+                        "T_MM_ADD_STOCK\n" +
+                        "T_MM_STOCK\n" +
+                        "T_MM_MATTER_CLASS\n" +
+                        "T_MM_INCOME_COST_STATISTICS\n" +
+                        "T_MM_INCOME_COST_DETAIL\n" +
+                        "T_MM_REPORT_FORM_INFO\n" +
+                        "T_MM_SECTION_SL_TEMP\n";
     }
     public static String template(String tableName, String EntityName) {
-        String temp = "\t<table tableName=\""+tableName+"\" domainObjectName=\""+EntityName+"\" enableDeleteByExample=\"false\"\n" +
+        String temp = "\t<table tableName=\""+tableName.trim()+"\" domainObjectName=\""+EntityName.trim()+"\" enableDeleteByExample=\"false\"\n" +
                 "\t\t\t   enableUpdateByExample=\"false\" selectByExampleQueryId=\"false\"\n" +
                 "\t\t\t   selectByPrimaryKeyQueryId=\"false\" enableCountByExample=\"false\" enableSelectByExample=\"false\">\n" +
                 "\t\t\t<property name=\"useActualColumnNames\" value=\"false\"/>\n" +
